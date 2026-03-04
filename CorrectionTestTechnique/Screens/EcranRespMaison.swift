5444c1c08432186f57971aab6d12ef5fb4dfb4a1
AyoubProGit <ayoub.abarji.pro@gmail.com>
2026-03-04 11:29:10 +0100

    Modification de student en class + ajout ecran Resp Maison

---
+++ CorrectionTestTechnique/Screens/EcranRespMaison.swift
@@ -0,0 +1,18 @@
+//
+//  EcranRespMaison.swift
+//  CorrectionTestTechnique
+//
+//  Created by Ayoub ABARJI on 04/03/2026.
+//
+
+import SwiftUI
+
+struct EcranRespMaison: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EcranRespMaison()
+}
